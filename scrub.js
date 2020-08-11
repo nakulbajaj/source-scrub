6ddd0e6f81189920dfc3de5a4848658496179d49
nakulbajaj <nakulbajaj@me.com>
2020-08-11 10:53:52 -0700

    Completed file upload functionality

--- scrub.js
+++ scrub.js
@@ -1,3 +1,5 @@
+ var downloadReady = false;
+ var source;
  /* Scrubs all instances of word from source.
   * Will match even if word is inside another word.
   */
@@ -41,40 +43,82 @@ function scrub_ignoring_delimiters(source, word, replacement){
 }
 
 function scrub(){
-	var source = document.getElementById('sourceInput').value;
-	var pii = document.getElementById('piiInput').value;
-	var output = document.getElementById('output');
-	var py_name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var address = document.getElementById('address').value;
-	var ssn = document.getElementById('ssn').value;
-	// Format the ssn into three separate parts
-	pattern = new RegExp('([0-9]{3})([0-9]{2})([0-9]{4})');
-	ssn = ssn.replace(pattern, '$1-$2-$3');
-	var voterID = document.getElementById('voterID').value;
-	var driverID = document.getElementById('driverID').value;
-	source = scrub_multi_word(source, py_name, 'NAME');
-	source = scrub_word(source, email, 'EMAIL');
-	source = scrub_word(source, encodeURI (email), 'EMAIL');
-	source = scrub_multi_word(source, address, 'ADDRESS');
-	source = scrub_ignoring_delimiters(source, ssn, 'SSN');
-	source = scrub_multi_word(source, voterID, 'voterID');
-	source = scrub_ignoring_delimiters(source, driverID, 'driverID')
-	for (var keyword of pii) {
-		var source = scrub_multi_word(source, keyword, 'PII');
-	}
-	output.value = source;
+	if(downloadReady == true){
+		console.log("ready");
+		//creating an invisible element 
+                var element = document.createElement('a'); 
+                element.setAttribute('href',  
+                'data:text/plain;charset=utf-8, ' 
+                + encodeURIComponent(source)); 
+                element.setAttribute('download', document.getElementById('sourceInput').value.substring(12)); 
+              
+                // Above code is equivalent to 
+                // <a href="path of file" download="file name"> 
+              
+                document.body.appendChild(element); 
+              
+                //onClick property 
+                element.click(); 
+              
+                document.body.removeChild(element); 
+	} else {
+		document.getElementById('scrubButton').innerText = "Scrubbing...";
+		let reader = new FileReader();
+		reader.readAsText(sourceInput.files[0]);
+		reader.onload = function(){
+	      var text = reader.result;
+	      source = reader.result;
+
+	      var sourceInput = document.getElementById('sourceInput').value;
+	      var pii = document.getElementById('piiInput').value;
+	      var py_name = document.getElementById('name').value;
+	      var email = document.getElementById('email').value;
+	      var address = document.getElementById('address').value;
+	      var ssn = document.getElementById('ssn').value;
+	      var voterID = document.getElementById('voterID').value;
+	      var driverID = document.getElementById('driverID').value;
+			// Format the ssn into three separate parts
+			pattern = new RegExp('([0-9]{3})([0-9]{2})([0-9]{4})');
+			ssn = ssn.replace(pattern, '$1-$2-$3');
+			source = scrub_multi_word(source, py_name, 'NAME');
+			source = scrub_word(source, email, 'EMAIL');
+			source = scrub_word(source, encodeURI (email), 'EMAIL');
+			source = scrub_multi_word(source, address, 'ADDRESS');
+			source = scrub_ignoring_delimiters(source, ssn, 'SSN');
+			source = scrub_multi_word(source, voterID, 'voterID');
+			source = scrub_ignoring_delimiters(source, driverID, 'driverID');
+			for (var keyword of pii) {
+				source = scrub_multi_word(source, keyword, 'PII');
+			}
+
+	  		document.getElementById('scrubButton').classList.replace('btn-primary', 'btn-success');
+	  		document.getElementById('scrubButton').innerText = "Download";
+
+	  		downloadReady = true;
+	  	}
+    };
 };
 
-function copyToClipboard(){
-	// var range = document.createRange();
-	// var selection = window.getSelection();
-	// range.selectNodeContents(document.getElementById('output'));
-	// selection.removeAllRanges();
-	// selection.addRange(range);
-	var output = document.getElementById('output')
-	output.select() 
-	document.execCommand("copy");
-
+function reset(){
+	downloadReady = false;
+	document.getElementById('sourceFilename').innerText = document.getElementById('sourceInput').files[0].name;
+	let sourceInput = document.getElementById('sourceInput');
+	let scrubButton = document.getElementById('scrubButton');
+	if(sourceInput.files.length > 0){
+		if(scrubButton.classList.contains("btn-light")){
+			scrubButton.classList.replace("btn-light", "btn-primary");
+		} else if (scrubButton.classList.contains("btn-success")){
+			scrubButton.classList.replace("btn-success", "btn-primary");
+		}
+		scrubButton.disabled = false;
+		scrubButton.innerText = "Scrub"
+	} else {
+		if(scrubButton.classList.contains("btn-primary")){
+			scrubButton.classList.replace("btn-primary", "btn-light");
+		} else if (scrubButton.classList.contains("btn-success")){
+			scrubButton.classList.replace("btn-success", "btn-light");
+		}
+		scrubButton.disabled = true;
+		scrubButton.innerText = "Upload File"
+	}	
 }
-
